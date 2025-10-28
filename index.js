81fc4feba8d68c4cd07b7598e48adc9ab6b3ca56
Nguyen Manh Dung <nguyenmanhdung23020520@gmail.com>
2025-10-28 22:59:16 +0700

    update cicd.yml

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from CI/CD demo!');
+  res.send('Hello Nguyen Manh Dung!');
 });
 
 app.listen(PORT, () => {
